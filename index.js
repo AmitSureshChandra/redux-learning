e56b4e45a71a3c78a19b601b2ad92eaa41bf75cd
Amit <akumar00029@gmail.com>
2021-09-16 09:57:14 +0530

    multiple state & reducer implemented

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ const BUY_ICECREAM = 'BUY_ICECREAM'
 
 // state 
 
-const initialState = () => {
+const initialCakeState = () => {
     return {
-        no_of_cake: 10,
+        no_of_cake: 10
+    }
+}
+
+const initialIcecreamState = () => {
+    return {
         no_of_icecream: 10
     }
 }
@@ -32,27 +37,36 @@ const buyIcecream = () => {
 
 // reducer
 
-const reducer = (state = initialState(), action) => {
+const cake_reducer = (state = initialCakeState(), action) => {
     switch(action.type){
         case BUY_CAKE:
             return {
                 ...state,
                 no_of_cake: state.no_of_cake - 1
             }
+        default: return state
+    }
+}
 
+const icecream_reducer = (state = initialIcecreamState(), action) => {
+    switch(action.type){
         case BUY_ICECREAM:
             return {
                 ...state,
                 no_of_icecream: state.no_of_icecream - 1
             }
-
         default: return state
     }
 }
 
 // redux
 
-const store = redux.createStore(reducer)
+const combineReducers = redux.combineReducers({
+    cake: cake_reducer,
+    ice_cream: icecream_reducer
+})
+
+const store = redux.createStore(combineReducers)
 
 console.log(store.getState());
 
