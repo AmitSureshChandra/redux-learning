f5a95e359f6be92db85f83201396534f4468a4ab
Amit <akumar00029@gmail.com>
2021-09-16 09:51:09 +0530

    one more action added

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import redux from 'redux'
 // action constants
 
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 // state 
 
 const initialState = () => {
     return {
-        no_of_cake: 10
+        no_of_cake: 10,
+        no_of_icecream: 10
     }
 }
 
@@ -21,6 +23,13 @@ const buyCake = () => {
     }
 }
 
+const buyIcecream = () => {
+    return {
+        type: BUY_ICECREAM,
+        info: 'Buy icecream'
+    }
+}
+
 // reducer
 
 const reducer = (state = initialState(), action) => {
@@ -30,6 +39,13 @@ const reducer = (state = initialState(), action) => {
                 ...state,
                 no_of_cake: state.no_of_cake - 1
             }
+
+        case BUY_ICECREAM:
+            return {
+                ...state,
+                no_of_icecream: state.no_of_icecream - 1
+            }
+
         default: return state
     }
 }
@@ -40,7 +56,15 @@ const store = redux.createStore(reducer)
 
 console.log(store.getState());
 
-store.dispatch(buyCake())
+store.dispatch(buyCake());
+
+console.log(store.getState());
+
+store.dispatch(buyIcecream());
+
+console.log(store.getState());
+
+store.dispatch(buyCake());
 
 console.log(store.getState());
 
